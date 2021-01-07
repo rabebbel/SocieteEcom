d791efcabdaeb14a86647ad15fb0804e941a1739
Rabeb <Rabeb@Rabeb-PC>
2021-01-07 10:29:06 +0100

    ff

---
+++ src/main/java/com/net/societe/ServiceImp/VilleServiceImp.java
@@ -0,0 +1,12 @@
+package com.net.societe.ServiceImp;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.net.societe.IService.VilleIService;
+
+@Service
+@Transactional
+public class VilleServiceImp implements VilleIService{
+
+}
